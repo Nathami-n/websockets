5ca2d3b1ce13c8494df14bccca2e6f968f433ea0
Nathan <georgenathan010@gmail.com>
2024-04-29 01:33:27 +0300

    Added the socket.io client and exposed an io global which connects to the host serving the page

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { createServer } from "node:http";
 import router from './routes/serverRouter.js';
+import { Server } from 'socket.io';
+
+
 const app = express();
 const server = createServer(app);
-
+const io = new Server(server);
 
 app.use('/', router);
 
+io.on('connection', (socket) => {
+    console.log("a user connected");
+})
 
 
 server.listen(3000, ()=> {
